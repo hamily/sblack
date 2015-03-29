85e561cb41e9fc469e18ee6a9a98b269ed4979b9
Sblack Wang <wyjun0317@163.com>
2015-03-29 19:26:01 +0800

    add yueai file

---
+++ yueai/yueai_android/application/yueai/androidzh/core/androidGuest.php
@@ -0,0 +1,10 @@
+<?php
+defined('YUEAI') or exit('Access Denied！'); 
+/**
+ * 此文件为android用户登录接口文件
+ * @param api={}
+ * @return array()
+**/
+
+$ = trim($_POST['api']);
+?>
---
+++ yueai/yueai_android/application/yueai/androidzh/core/config/core.system.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 此类系统内相关配置
+**/
+defined('YUEAI') or exit('Access Denied！');
+
+class Core_System {
+	
+	
+	
+}
+?>
